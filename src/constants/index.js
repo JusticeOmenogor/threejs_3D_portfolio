023e4346be3d7908c594172c186bc15c68fd311f
Justice Omenogor <justiceomenogor@gmail.com>
2023-12-14 10:36:38 +0100

    services, About, SectionWrapper

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Machine Learning Engineer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Data Analyst",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Project Manager",
       icon: creator,
     },
   ];
